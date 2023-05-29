b4874f955b8038339a31eb65525dd8096854d453
andreacassini <andrea1316@hotmail.it>
2023-05-29 13:59:13 +0200

    Add style to buttons sliders, ciclo img

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 //ARRAY IMMAGINI
-let imgList = ['assets/./img/01.webp','assets/./img/02.webp','assets/./img/03.webp','assets/./img/04.webp','assets/./img/05.webp']
+let picsList = ['assets/./img/01.webp','assets/./img/02.webp','assets/./img/03.webp','assets/./img/04.webp','assets/./img/05.webp']
 
 //VAR sliderElem
+let sliderElem = document.querySelector('.slider');
 
+let sliderImages = "";
+
+for (let i = 0; i < picsList.length; i++){
+
+    let pictures = picsList[i];
+
+    sliderImages += `
+    <div class="slide">
+        <img src="${pictures}" alt="slide-${i+1}">
+    </div>`
+}
+
+sliderElem.innerHTML += sliderImages;
+
+let slide = document.querySelectorAll('.slide');
+
+let activeSlider = 0;
+
+slide[activeSlider].classList.add('active');
